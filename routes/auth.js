9a29dc6d17e58b3277d9338f9e450028b05ccf67
ManuxDark <mgcastelberg@hotmail.com>
2025-06-22 21:01:35 -0600

    Custom Middlewares

--- routes/auth.js
+++ routes/auth.js
@@ -8,10 +8,12 @@ const { check } = require('express-validator');
 const router = Router();
 
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 router.post('/',[
     check('email', 'Email is required').trim().isEmail(),
-    check('password', 'Password must be at least 6 characters long').trim().isLength({ min: 6 })
+    check('password', 'Password must be at least 6 characters long').trim().isLength({ min: 6 }),
+    validarCampos
 ], loginUser);
 
 router.post('/renew', renewToken);
@@ -20,7 +22,8 @@ router.post('/register',
     [   // Middlewares
         check('name', 'Name is required').not().isEmpty(),
         check('email', 'Email is required').isEmail(),
-        check('password', 'Password must be at least 6 characters long').isLength({ min: 6 })
+        check('password', 'Password must be at least 6 characters long').isLength({ min: 6 }),
+        validarCampos
     ], createUser);
 
 module.exports = router;
