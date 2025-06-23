b632352ee89925cd6970224ad1c7eada652261f3
ManuxDark <mgcastelberg@hotmail.com>
2025-06-22 20:34:01 -0600

    Express Validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,19 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const loginUser = (req, res = response) => {
 
     const { email, password } = req.body;
+
+    // Manejo de errores
+    const errors = validationResult(req);
+    if ( !errors.isEmpty() ) {
+        return res.status(400).json({
+            status: false,
+            errors: errors.mapped()
+        });
+    }
+
     res.json({
         status:true,
         msg: 'Login...',
@@ -16,6 +27,15 @@ const loginUser = (req, res = response) => {
 const createUser = (req, res = response) => {
 
     const { name, email, password } = req.body;
+
+    // Manejo de errores
+    const errors = validationResult(req);
+    if ( !errors.isEmpty() ) {
+        return res.status(400).json({
+            status: false,
+            errors: errors.mapped()
+        });
+    }
 
     if ( password.length < 6 ) {
         return res.status(400).json({
--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,23 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
 
-router.post('/', loginUser);
+router.post('/',[
+    check('email', 'Email is required').trim().isEmail(),
+    check('password', 'Password must be at least 6 characters long').trim().isLength({ min: 6 })
+], loginUser);
 
 router.post('/renew', renewToken);
 
-router.post('/register', createUser);
+router.post('/register',
+    [   // Middlewares
+        check('name', 'Name is required').not().isEmpty(),
+        check('email', 'Email is required').isEmail(),
+        check('password', 'Password must be at least 6 characters long').isLength({ min: 6 })
+    ], createUser);
 
 module.exports = router;
