6e9ddc39af26c1e8d23b02855f182062eb08dfcb
ManuxDark <mgcastelberg@hotmail.com>
2025-06-28 13:40:36 -0600

    Generar un Json Web Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,51 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
+const { generateJWT } = require('../helpers/jwt');
 const Usuario = require('../model/Usuario'); // mongoose
 const User = require('../model/User_model'); // sequelize
 
-const loginUser = (req, res = response) => {
+const loginUser = async(req, res = response) => {
 
     const { email, password } = req.body;
 
-    res.json({
-        status:true,
-        msg: 'Login...',
-        user: {
-            email,
-            password
+    try {
+
+        const user = await User.findOne({ where: { email: email } });
+
+        if ( !user ) {
+            return res.status(400).json({
+                status:false,
+                msg: 'User not found'
+            });
         }
-    });
+
+        const validPassword = bcrypt.compareSync(password, user.password); //True or False
+        if ( !validPassword ) {
+            return res.status(400).json({
+                status:false,
+                msg: 'Password incorrect'
+            });
+        }
+
+        // Generar JWT
+        const token = await generateJWT(user.id, user.name);
+
+        res.json({
+            status:true,
+            msg: 'Login...',
+            user: {
+                uid: user.id,
+                name: user.name,
+                token
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+                status:false,
+                msg: 'Please contact the administrator'
+        });
+    }
 }
 
 const createUser = async(req, res = response) => {
@@ -48,12 +79,16 @@ const createUser = async(req, res = response) => {
             });
         }
         newUser = await User.create({ name, email, password: passwordHashed }); // sequelize
+
+        // Generar JWT
+        const token = await generateJWT(newUser.id, newUser.name);
     
         res.status(201).json({
             status:true,
             msg: 'Register',
             uid: newUser.id,
-            name: newUser.name
+            name: newUser.name,
+            token
         });
     } catch (error) {
         console.log(error);
